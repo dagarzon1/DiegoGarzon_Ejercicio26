1f04826e6a597ba1899fb8c6170159749897d7a5
dagarzon1 <37335375+dagarzon1@users.noreply.github.com>
2018-11-08 20:08:41 -0500

    Update DiegoGarzon.sh

--- DiegoGarzon.sh
+++ DiegoGarzon.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-#SBATCH -J myjob
-#SBATCH -o myjob.o%j
-#SBATCH -e myjob.e%j
-#SBATCH –N 2
-#SBATCH -n 2
-#SBATCH -p development
-#SBATCH -t 01:30:00 #
+!/bin/bash
 
+#PBS -l nodes=1;intel:ppn=8, mem=64gb
+#PBS -l walltime=00:05:00
+#PBS -M da.garzon1@uniandes.edu.co
+#PBS -m abe
+#PBS -N DiegoGarzon_Ejercicio25
+#PBS -j oe
 
-ibrun make
+module load anaconda/python3
+cd da.garzon1/DiegoGarzon_Ejercicio26
